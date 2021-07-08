436f53145ff04f0a62128b5534ed4a5033619841
cjx <757672398@qq.com>
2021-07-08 19:37:45 +0800

    first commit

---
+++ src/main/java/com/cjx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cjx.git;
+
+/**
+ * @author cjx
+ * @date 2021/7/8 0008 - 19:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
