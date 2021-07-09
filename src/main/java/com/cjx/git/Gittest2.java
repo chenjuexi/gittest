5b4954257d389409a4aa9f7f59d8a25c23c0b481
cjx <757672398@qq.com>
2021-07-09 13:15:38 +0800

    gittest2 commit

---
+++ src/main/java/com/cjx/git/Gittest2.java
@@ -0,0 +1,11 @@
+package com.cjx.git;
+
+/**
+ * @author cjx
+ * @date 2021/7/9 0009 - 13:14
+ */
+public class Gittest2 {
+    public static void main(String[] args) {
+        System.out.println("gittest2");
+    }
+}
