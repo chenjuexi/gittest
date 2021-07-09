1c03b19b242c0f7946e5c976a1d0c6be14237ba3
chenjuexi <87118259+chenjuexi@users.noreply.github.com>
2021-07-09 13:43:30 +0800

    pull commit

--- src/main/java/com/cjx/git/GitTest.java
+++ src/main/java/com/cjx/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("hello hot-fix2");
         System.out.println("hello master2");
+         System.out.println("pull test");
     }
 }
